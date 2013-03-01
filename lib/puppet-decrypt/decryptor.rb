1c7251e04c27e212845a4f5237b5db13a5173b2b
mlincoln <mlincoln@thoughtworks.com>
2013-03-01 15:14:52 -0300

    Ruby 1.8.7 support

--- lib/puppet-decrypt/decryptor.rb
+++ lib/puppet-decrypt/decryptor.rb
@@ -11,10 +11,37 @@ module Puppet
         secret_key_digest = Digest::SHA256.hexdigest(secret_key)
         if value =~ Puppet::Decrypt::ENCRYPTED_PATTERN
           value = $~[1]
-          value = Base64.strict_decode64(value)
-          value = value.decrypt(key: secret_key_digest)
+          value = strict_decode64(value)
+          value = value.decrypt(:key => secret_key_digest)
         end
         value
+      end
+
+      # Backported for ruby 1.8.7 
+      #
+      # Decodes a strictly base64 encoded String.
+      #
+      # @param [String] str
+      #   The strictly encoded base64 String.
+      #
+      # @return [String]
+      #   The decoded String.
+      #
+      # @raise [ArgumentError]
+      #   The String is incorrectly padded or contains non-alphabet characters.
+      #   Note: CR or LF are also rejected.
+      #
+      # @note
+      #   This method complies with RFC 4648.
+      #
+      def self.strict_decode64(str)
+        return Base64.strict_decode64(str) if Base64.respond_to? :base64
+
+        unless str.include?("\n")
+          decode64(str)
+        else
+          raise(ArgumentError,"invalid base64")
+        end
       end
     end
   end
