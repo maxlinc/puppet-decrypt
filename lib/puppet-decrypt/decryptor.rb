0ec9112fea81dbf738900d23147c9852bb1827d2
mlincoln <mlincoln@thoughtworks.com>
2013-03-07 23:38:12 -0300

    Backport strict_encode64 for ruby 1.8

--- lib/puppet-decrypt/decryptor.rb
+++ lib/puppet-decrypt/decryptor.rb
@@ -18,42 +18,34 @@ module Puppet
 
       def self.encrypt(value)
         result = value.encrypt(:key => secret_key_digest)
-        encrypted_value = Base64.strict_encode64(result).strip
+        encrypted_value = strict_encode64(result).strip
         raise "Value can't be encrypted properly" unless decrypt(encrypted_value, true) == value
         encrypted_value
       end
 
       private
 
-        def self.secret_key_digest
-          Digest::SHA256.hexdigest(secret_key)
-        end
-        # Backported for ruby 1.8.7 
-        #
-        # Decodes a strictly base64 encoded String.
-        #
-        # @param [String] str
-        #   The strictly encoded base64 String.
-        #
-        # @return [String]
-        #   The decoded String.
-        #
-        # @raise [ArgumentError]
-        #   The String is incorrectly padded or contains non-alphabet characters.
-        #   Note: CR or LF are also rejected.
-        #
-        # @note
-        #   This method complies with RFC 4648.
-        #
-        def self.strict_decode64(str)
-          return Base64.strict_decode64(str) if Base64.respond_to? :strict_decode64
+      def self.secret_key_digest
+        Digest::SHA256.hexdigest(secret_key)
+      end
 
-          unless str.include?("\n")
-            Base64.decode64(str)
-          else
-            raise(ArgumentError,"invalid base64")
-          end
+      # Backported for ruby 1.8.7 
+      def self.strict_decode64(str)
+        return Base64.strict_decode64(str) if Base64.respond_to? :strict_decode64
+
+        unless str.include?("\n")
+          Base64.decode64(str)
+        else
+          raise(ArgumentError,"invalid base64")
         end
+      end
+
+      # Backported for ruby 1.8.7
+      def self.strict_encode64(bin)
+        return Base64.strict_encode64(bin) if Base64.respond_to? :strict_encode64
+        Base64.encode64(bin).tr("\n",'')
+      end
+
     end
   end
 end
