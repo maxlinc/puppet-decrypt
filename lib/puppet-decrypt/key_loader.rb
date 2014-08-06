ce25f93a9795188be6dc57fff0c322e2339aef51
Tio Teath <tioteath@gmail.com>
2014-08-06 10:09:37 +0300

    Add encrypt function. Minor changes.

--- lib/puppet-decrypt/key_loader.rb
+++ lib/puppet-decrypt/key_loader.rb
@@ -1,9 +1,27 @@
+require 'pathname'
 module Puppet
   module Decrypt
     class KeyLoader
       def load_key(secret_key_file)
-        raise "Secret key file: #{secret_key_file} is not readable!" unless File.readable?(secret_key_file)
-        secret_key = File.open(secret_key_file, &:readline).chomp
+
+        # Dot not add directory if absolute path provided
+        if Pathname.new(secret_key_file).absolute?
+          full_path = secret_key_file
+        else
+          full_path = File.join(ENV['PUPPET_DECRYPT_KEYDIR'] ||
+              Puppet::Decrypt::Decryptor::KEY_DIR, secret_key_file)
+        end
+
+        # Assume key file is specified as basename
+        if File.readable? full_path
+          secret_key_file = full_path
+        else
+          # Assume key file is specifed as full path
+          raise "Secret key file: #{secret_key_file} is not readable!" unless
+              File.readable? secret_key_file
+        end
+
+        File.open(secret_key_file, &:readline).chomp
       end
     end
   end
--- lib/puppet/parser/functions/decrypt.rb
+++ lib/puppet/parser/functions/decrypt.rb
@@ -4,12 +4,15 @@ module Puppet::Parser::Functions
   newfunction(:decrypt, :type => :rvalue) do |args|
     options = {}
     decrypt_args = {}
-    if args[0].is_a? String
-      decrypt_args['value'] = args[0]
+
+    if args.first.is_a? String
+      decrypt_args['value'], decrypt_args['secret_key'] = args
+    elsif args.first.is_a? Hash
+      decrypt_args = args.first
     else
-      decrypt_args = args[0]
-      puts "Using hash: #{decrypt_args}"
+      raise TypeError, "Excpected String or Hash, given #{args.first.class}"
     end
+
     Puppet::Decrypt::Decryptor.new(options).decrypt_hash(decrypt_args)
   end
 end
